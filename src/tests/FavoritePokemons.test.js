97b9c2e5dea55e1571d79a3134540c86f9eec2ab
Bruna Santiago <brunasanthiago@outlook.com>
2022-06-08 14:26:29 -0300

    teste 2, requisito 3

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -2,11 +2,21 @@ import React from 'react';
 import { screen } from '@testing-library/react';
 import renderWithRouter from '../pages/renderWithRouter';
 import App from '../App';
+import FavoritePokemons from '../pages/FavoritePokemons';
 
 describe('Testar o componente Favorite Pokemons', () => {
   test('Exibe No favorite pokemon found, caso não tenha pokémons favoritos', () => {
+    renderWithRouter(<FavoritePokemons />);
+    const favorite = screen.getByText(/No favorite pokemon found/i);
+    expect(favorite).toBeInTheDocument();
+  });
+  test('Teste se são exibidos todos os cards de pokémons favoritados', () => {
     renderWithRouter(<App />);
-    const favorite = screen.getByRole('heading', { name: /No favorite pokemon found/i });
-    expect(favorite).toHaveBeenCalled();
+    const name = screen.getByTestId('pokemon-name');
+    expect(name).toBeInTheDocument();
+    const type = screen.getByTestId('pokemon-type');
+    expect(type).toBeInTheDocument();
+    const weight = screen.getByTestId('pokemon-weight');
+    expect(weight).toBeInTheDocument();
   });
 });
